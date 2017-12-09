fbf9ec43ea1050efff5ef84966b43798a327a60a
Greg de Valois <gdv@devicenut.com>
2017-12-09 11:44:17 -0800

    Initial commit.

---
+++ NeoPixelShow.h
@@ -0,0 +1,50 @@
+/*--------------------------------------------------------------------
+  This file is part of the Adafruit NeoPixel library.
+
+  NeoPixel is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
+
+  NeoPixel is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with NeoPixel.  If not, see
+  <http://www.gnu.org/licenses/>.
+
+  This minimal library only supports GRB data ordering, 800 KHz streaming,
+  and a modern version of the Arduino infrastructure. It does not support
+  the ESP8266 or ESP32 processors.
+
+  --------------------------------------------------------------------*/
+
+#ifndef NEOPIXEL_SHOW_H
+#define NEOPIXEL_SHOW_H
+
+ #include <Arduino.h>
+
+class NeoPixelShow {
+
+ public:
+
+  // Constructor: pin number
+  NeoPixelShow(uint8_t p);
+
+  void show(uint8_t *pixels, uint16_t numBytes);
+  inline bool canShow(void) { return (micros() - endTime) >= 50L; }
+
+ private:
+
+  int8_t pin;               // Output pin number
+  uint32_t endTime;         // Latch timing reference
+
+#ifdef __AVR__
+  volatile uint8_t *port;   // Output PORT register
+  uint8_t pinMask;          // Output PORT bitmask
+#endif
+};
+
+#endif // NEOPIXEL_SHOW_H
---
+++ README.md
@@ -0,0 +1,9 @@
+# NeoPixelShow Library
+
+Arduino library for controlling single-wire-based LED pixels (such as the WS2812B) and strips as can be found from Adafruit (www.adafruit.com) or other retailers.
+
+This has been reduced to the bare minimum show() function, offloading all other functionality to the calling application code, for maximum flexibility.
+
+It supports AVR and ARM processors used in standard Arduino modules, but NOT the ESP8266 or ESP32 processors.
+
+After downloading, rename folder to 'NeoPixelShow' and install in the Arduino Libraries folder.
